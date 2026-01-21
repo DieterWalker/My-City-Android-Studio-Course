e9d3d769631adf93c6a54359f7f86a89d3d9570b
Dieter Walker <nguyenquocdue32@gmail.com>
2026-01-21 10:16:39 +0700

    update home screen

---
+++ app/src/main/java/com/example/mycityandroidstudiocourses/ui/MyCityApp.kt
@@ -0,0 +1,9 @@
+/*  */
+package com.example.mycityandroidstudiocourses.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyCityApp(){
+    MyCityHomeScreen()
+}
---
+++ app/src/main/java/com/example/mycityandroidstudiocourses/ui/MyCityViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.mycityandroidstudiocourses.ui
+
+import androidx.lifecycle.ViewModel
+
+class MyCityViewModel : ViewModel() {
+}
